c9d05c9f01af89ca2aaa311484d6edd14407f480
GuangyanS <george20020904@gmail.com>
2024-05-02 12:31:47 -0400

    full param finetune

--- SwiLLaVA/scripts/finetune.sh
+++ SwiLLaVA/scripts/finetune.sh
@@ -1,28 +1,20 @@
-#!/bin/bash
-
-################## VICUNA ##################
-PROMPT_VERSION=v1
-MODEL_VERSION="vicuna-v1-3-7b"
-################## VICUNA ##################
-
 deepspeed llava/train/train_mem_search.py \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path lmsys/vicuna-7b-v1.3 \
-    --version $PROMPT_VERSION \
-    --data_path /path/to/seal_vqa_data \
-    --image_folder /path/to/data \
+    --model_name_or_path liuhaotian/llava-v1.5-7b \
+    --version v1 \
+    --data_path /home/dxleec/gysun/datasets/seal_vqa_data \
+    --image_folder /home/dxleec/gysun/datasets \
     --vision_tower openai/clip-vit-large-patch14 \
-    --mm_projector_type linear \
+    --mm_projector_type mlp2x_gelu \
     --object_mm_projector_type perceiver \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-linear-pretrain/mm_projector.bin \
-    --pretrain_mm_perceiver_adapter ./checkpoints/llava-$MODEL_VERSION-resampler-pretrain/mm_projector.bin \
+    --pretrain_mm_perceiver_adapter /home/dxleec/gysun/init_weights/LISA-Results/llava-vicuna-v1-5-7b-resampler-pretrain/mm_projector.bin \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --bf16 True \
-    --output_dir ./checkpoints/seal_vqa_7b \
-    --num_train_epochs 2 \
+    --output_dir ./checkpoints/switch-vqa-7b \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
@@ -40,4 +32,5 @@ deepspeed llava/train/train_mem_search.py \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    --report_to wandb
+    --report_to wandb \
+    --run_name 'swi-7b-finetune'
