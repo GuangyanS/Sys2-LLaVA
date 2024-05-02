ba8865b72f290e09b07d2eb6ecb9e84743022b57
GuangyanS <george20020904@gmail.com>
2024-05-02 12:41:38 -0400

    cos full param

--- ROILLaVA/scripts/v1_5/finetune_CoS_7b.sh
+++ ROILLaVA/scripts/v1_5/finetune_CoS_7b.sh
@@ -1,19 +1,12 @@
-GPU=$[$WORLD_SIZE * 8]
-echo GPU=${GPU}
-echo WORLD_SIZE=$WORLD_SIZE
-echo RANK=$RANK
-echo MASTER_ADDR=$MASTER_ADDR
-echo MASTER_PORT=$MASTER_PORT
 
-WANDB_MODE=offline deepspeed llava/train/train_mem_CoS.py \
+deepspeed llava/train/train_mem_CoS.py \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path ./checkpoints/llava-v1.5-7b-ckpt \
+    --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version v1 \
     --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --cos_data_path /PATH/TO/YOUR/DATA \
+    --cos_data_path /home/gs3260/gysun/datasets/LLaVA-Instruct-150K/Chain-of-Spot.json \
     --image_folder ./playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-ckpt/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -21,7 +14,7 @@ WANDB_MODE=offline deepspeed llava/train/train_mem_CoS.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b-CoS \
+    --output_dir ./checkpoints/cos-v1.5-7b \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -30,7 +23,7 @@ WANDB_MODE=offline deepspeed llava/train/train_mem_CoS.py \
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
-    --learning_rate 2e-5 \
+    --learning_rate 2e-4 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -40,4 +33,5 @@ WANDB_MODE=offline deepspeed llava/train/train_mem_CoS.py \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    --report_to wandb
+    --report_to wandb \
+    --run_name cos-7b'
